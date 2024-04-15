d9fc317ac14f20e4981982d20054d4f8c0eeb13d
FauzanYanuarPutra <ccdplayerx@gmail.com>
2024-04-15 08:52:34 +0700

    first commit

---
+++ src/views/DateView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="date">
+    <v-date-picker show-adjacent-months v-model="date"></v-date-picker>
+    {{ date }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const date = ref(new Date());
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,136 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <v-card class="pa-8 d-flex justify-center align-center flex-wrap h-screen" theme="dark">
+    <v-container class="text-center">
+      <v-row justify="center" dense>
+        <v-col cols="12">
+          <v-img
+            class="mx-auto mt-12 mb-16"
+            max-height="140"
+            max-width="240"
+            src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-dark-text.svg"
+          ></v-img>
+          <!-- <h1 class="mx-auto mt-12 mb-16 text-uppercase">Belajar Vuetify</h1> -->
+        </v-col>
+
+        <v-col cols="12">
+          <v-autocomplete
+            :items="items"
+            append-inner-icon="mdi-microphone"
+            class="mx-auto"
+            density="comfortable"
+            menu-icon=""
+            placeholder="Search Google or type a URL"
+            prepend-inner-icon="mdi-magnify"
+            style="max-width: 350px;"
+            theme="light"
+            variant="solo"
+            auto-select-first
+            item-props
+            rounded
+          ></v-autocomplete>
+        </v-col>
+
+        <v-col
+          v-for="(shortcut, i) in shortcuts"
+          :key="i"
+          cols="auto"
+        >
+          <v-card
+            :href="shortcut.href"
+            class="pa-4"
+            rel="noopener noreferer"
+            target="_blank"
+            width="112"
+            flat
+          >
+            <v-avatar :icon="shortcut.icon" class="mb-2" color="white" variant="tonal"></v-avatar>
+
+            <div class="text-caption text-truncate" v-text="shortcut.title"></div>
+          </v-card>
+        </v-col>
+
+        <v-col cols="auto">
+          <v-dialog v-model="dialog" max-width="500">
+            <template v-slot:activator="{ props }">
+              <v-card v-bind="props" class="pa-4" width="112" flat>
+
+                <v-avatar class="mb-2" color="white" icon="mdi-plus" variant="tonal"></v-avatar>
+
+                <div class="text-caption text-truncate">Add shortcut</div>
+              </v-card>
+            </template>
+
+            <v-card rounded="lg" title="Add shortcut">
+              <template v-slot:text>
+                <v-label class="text-caption">Name</v-label>
+
+                <v-text-field density="compact" variant="solo-filled" flat></v-text-field>
+
+                <v-label class="text-caption">URL</v-label>
+
+                <v-text-field density="compact" variant="solo-filled" flat></v-text-field>
+              </template>
+
+              <div class="py-4 px-5 text-end">
+                <v-btn
+                  class="text-none me-2"
+                  color="blue"
+                  text="Cancel"
+                  variant="text"
+                  border
+                  @click="dialog = false"
+                ></v-btn>
+
+                <v-btn
+                  class="text-none"
+                  color="blue"
+                  text="Done"
+                  variant="flat"
+                  @click="dialog = false"
+                ></v-btn>
+              </div>
+            </v-card>
+          </v-dialog>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
+<script>
+  export default {
+    data: () => ({
+      dialog: false,
+      items: [
+        {
+          prependIcon: 'mdi-clock-outline',
+          title: 'recipe with chicken',
+        },
+        {
+          prependIcon: 'mdi-clock-outline',
+          title: 'best hiking trails near me',
+        },
+        {
+          prependIcon: 'mdi-clock-outline',
+          title: 'how to learn a new language',
+        },
+        {
+          prependIcon: 'mdi-clock-outline',
+          title: 'DIY home organization ideas',
+        },
+        {
+          prependIcon: 'mdi-clock-outline',
+          title: 'latest fashion trends',
+        },
+      ],
+      shortcuts: [
+        {
+          icon: 'mdi-calendar',
+          title: 'Date ',
+          href: '/date',
+        },
+      
+      ],
+    }),
+  }
 </script>
